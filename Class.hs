8d8f01f07d7a84134ced4a41ae80721a6b90b616
iZyTeHPariaH <izyhq@openmailbox.org>
2015-09-19 15:09:58 +0200

    class

--- Class.hs
+++ Class.hs
@@ -1,24 +1,31 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
 module Class where
 
 import FRP.Sodium
 import qualified Data.Map as M
+import Control.Monad
 
 type Handler a = a -> Reactive ()
 
 data EventMapEntry e = EventMapEntry { eFire :: Handler e,
-                                       eEvent :: Event e}
+                                       eEvent :: Event e,
+                                       eFilter :: e -> Bool}
 data EventMap k e = EventMap {eventMap :: M.Map k (EventMapEntry e)}
 
 type EventManager id k e = M.Map id (Behaviour (EventMap k e))
+
+
+class (Ord k) => IDable e k | e -> k where
+        extractID :: e -> k
 
 {-| Removes the entry from the event map |-}
 deleteKey :: (Ord k) => k -> EventMap k e -> EventMap k e
 deleteKey k (EventMap m) = EventMap (M.delete k m)
 
 {-| Fires the value on the specified key event |-}
-fireKey :: (Ord k) => EventMap k e -> k -> e -> Maybe (Reactive ())
-fireKey m k e = ($e) . eFire <$> k `M.lookup` eventMap m
+fireKey :: (IDable e k) => EventMap k e -> e -> Maybe (Reactive ())
+fireKey m e = join $ f <$> extractID e `M.lookup` eventMap m
+  where f entry = if eFilter entry e then Just (eFire entry e) else Nothing
 
 
 {-| Extracts the event stream from a given key |-}
@@ -37,8 +44,6 @@ idEvents mana id = switchE . fmap allEvents' <$> idMapM
 {-| Merges all the events of a given manager |-}
 allEvents :: (Ord id, Ord k) => EventManager id k e -> Event e
 allEvents mana = foldr merge never $ switchE . fmap allEvents' <$> M.elems mana
-
-
 
 
 
