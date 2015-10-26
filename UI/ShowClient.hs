b4fe6f012365f67c448f16b3fe57e7f6c5f118e4
iZyTeHPariaH <izyhq@openmailbox.org>
2015-10-26 19:11:54 +0100

    recv

--- UI/ShowClient.hs
+++ UI/ShowClient.hs
@@ -23,18 +23,19 @@ renderClients cEventsL = buildApp $ zip (map show [1..]) (concatMap renderClient
 
 renderClient :: ClientInterface -> [A.Array (Int,Int) (AddHandler String)] --[A.Array (Int,Int) (AddHandler String)]
 renderClient cInterface = [win1,win2,win3, win4]
-    where [neigh,rLoc,rRel,pMap,resLocal, resListen, output, routLog, pipeLog] = showClientEvent' cInterface
+    where [neigh,rLoc,rRel,pMap,resLocal, resListen, output, input, routLog, pipeLog] = showClientEvent' cInterface
           win1 = A.array ((1,1),(2,2)) $ [((1,1),rLoc),((1,2),rRel),
                                             ((2,1),neigh), ((2,2),pMap)]
           win2 = A.listArray ((1,1),(2,1)) $ [resLocal, resListen] --, output, routLog]
           win3 = A.listArray ((1,1),(2,1)) $ [pipeLog, routLog]
-          win4 = A.listArray ((1,1),(1,1)) $ [output]
+          win4 = A.listArray ((1,1),(2,1)) $ [output,input]
 showClientEvent' :: ClientInterface -> [AddHandler String]
 showClientEvent' cInterface = [dump "NEIGHBORS" cleNeighborsMap, dump "ROUTING LOCAL" cleRoutingLocalMap, dump "ROUTING RELAY" cleRoutingRelayedMap]
                         ++ [showPipeMap <$> clePipeManager (ciEvents cInterface)] ++ [dump "RESSOURCE LOCAL" cleResLocalMap, dump "RESSOURCE LISTEN" cleResListenMap]
-                        ++ [interpretAsHandler (accumStrings 20) $ showPacket  <$> ciOutput cInterface, 
-                           interpretAsHandler (accumStrings 20) $ cleRoutingLogs $ ciEvents cInterface, 
-                           interpretAsHandler (accumStrings 20) $ clePipeLogs $ ciEvents cInterface]
+                        ++ [interpretAsHandler (accumStrings "SEND" 20) $ showPacket  <$> ciOutput cInterface, 
+                           interpretAsHandler (accumStrings "RECV" 20) $ showPacket <$> cleReceivedMessage (ciEvents cInterface),
+                           interpretAsHandler (accumStrings "ROUTING LOGS" 20) $ cleRoutingLogs $ ciEvents cInterface, 
+                           interpretAsHandler (accumStrings "PIPES LOGS" 20) $ clePipeLogs $ ciEvents cInterface]
     where dump name f = showMapKeys name <$> f (ciEvents cInterface)
 
 
@@ -47,8 +48,8 @@ showPipeMap m = unlines $ ("PIPES\n\n":map f (M.toList m))
 
           
 
-accumStrings :: Int -> Event t String -> Event t String
-accumStrings n e = concat <$> (accumE [] $ f <$> e)
+accumStrings :: String -> Int -> Event t String -> Event t String
+accumStrings name n e = ((name ++ "\n") ++) . concat <$> (accumE [] $ f <$> e)
   where f s l = take n $ (s ++ "\n") : l
 showPacket :: Packet -> String
 showPacket (Left (Left a)) = show a
