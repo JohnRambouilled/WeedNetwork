f05e21934817ebd2c29c4d1fde13c8e2ea20add0
iZyTeHPariaH <izomorphism@gmail.com>
2017-01-22 18:37:00 +0100

    packets

--- Client.hs
+++ Client.hs
@@ -1,15 +1,12 @@
 module Client where
 
 
-import Client.Crypto
-import Client.Neighbours
-import Client.Pipes
-import Client.Timer
-import Client.Graph
-import Client.Destinary
-import Client.Communication
-import Client.Relay
-
-
-
+import           Client.Communication
+import           Client.Crypto
+import           Client.Destinary
+import           Client.Graph
+import           Client.Neighbours
+import           Client.Pipes
+import           Client.Relay
+import           Client.Timer
 
--- Packets/Layer1.hs
+++ Packets/Layer1.hs
@@ -1,48 +1,50 @@
-{-# LANGUAGE DeriveGeneric, FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Packets.Layer1 where
 
-import Types.Crypto
-import Types.Packets
-import Packets.Layer2
-import Packets.Communication
+import           Control.Lens
+import           Packets.Communication
+import           Packets.Layer2
+import           Types.Crypto
+import           Types.Packets
 
-import Data.Binary
-import qualified Data.ByteString.Lazy as B
-import Data.Binary.Put
-import Data.Binary.Get
-import Data.Functor
-import GHC.Generics
-import qualified Data.Map as M
+import           Data.Binary
+import           Data.Binary.Get
+import           Data.Binary.Put
+import qualified Data.ByteString.Lazy  as B
+import           Data.Functor
+import qualified Data.Map              as M
+import           GHC.Generics
 
 neighTimeOut = 15 :: Time
 neighRepeatTime = 1 :: Time
 
 
-data L1Dest = Broadcast | DestKeyHash KeyHash
 
-data L1 = L1 {_l1Source :: KeyHash,
-              _l1Dest :: L1Dest,
-              _l1Content :: L1Content}
+
+data L1 = L1Intro NeighIntro | L1Data NeighData | L1Pipe PipePacket
     deriving Generic
-makeLenses ''L1
 
-data L1Content = L1Intro NeighIntro | L1Data NeighData | L1Pipe PipePacket
+
+
+data NeighIntro = NeighIntro {_neighISource :: KeyHash,
+                              _neighIPubKey :: PubKey,
+                              _neighISig    :: Signature}
     deriving Generic
-makeLenses ''L1Content
 
-
-data NeighIntro = NeighIntro {_neighIPubKey :: PubKey,  _neighISig :: Signature} 
+data NeighData = NeighData  {_neighDSource   :: KeyHash,
+                             _neighDestinary :: KeyHash,
+                             _neighDSig      :: Signature,
+                             _neighDContent  :: L2}
     deriving Generic
-makeLenses ''NeighIntro
-data NeighData = NeighData  {_neighDSig :: Signature, _neighDContent :: L2} 
-    deriving Generic
-makeLenses ''NeighData
 
-
-data PipePacket = PipePacket{_pipePacketHeader :: PipeHeader,
-                             _pipePacketData :: RawData }
+data PipePacket = PipePacket{ _pipeSource       :: KeyHash,
+                              _pipeDestinary    :: KeyHash,
+                              _pipePacketHeader :: PipeHeader,
+                             _pipePacketData    :: RawData }
                     deriving Show
-makeLenses ''PipePacket
+
 
 data PipePacketContent = PPCPipePacket PipePacket |
                          PPCL2 L2 |
@@ -51,52 +53,55 @@ data PipePacketContent = PPCPipePacket PipePacket |
 
 
 data PipeHeader = PipeHeader {_pipeDKeyID :: PipeID,  -- ^PipeID of the pipe used
-                              _pipeDSig :: Signature,  -- ^ Signature of the packet
+                              _pipeDSig   :: Signature,  -- ^ Signature of the packet
                               _pipeDFlags :: [PipeDataFlag] }
               deriving (Generic, Show)
-makeLenses ''PipeHeader
 
 data PipeDataFlag = PipeControlRefresh
                   | PipeClose RawData
               deriving (Show, Generic)
-
-instance Binary L1 
+makeLenses ''PipeHeader
+makeLenses ''PipePacket
+makeLenses ''NeighData
+makeLenses ''L1
+instance Binary L1
 instance Binary NeighData
 instance Binary NeighIntro
-instance Binary PipePacketContent 
+instance Binary PipePacketContent
 
-instance Binary PipePacket where put (PipePacket h c) = put h >> putLazyByteString c
-                                 get = PipePacket <$> get <*> getRemainingLazyByteString
+instance Binary PipePacket where put (PipePacket src dst h c) = put src >> put dst >> put h >> putLazyByteString c
+                                 get = PipePacket <$> get <*> get <*> get <*> getRemainingLazyByteString
 
 instance Binary PipeHeader
 instance Binary PipeDataFlag
 
 instance SignedClass NeighIntro where scHash (NeighIntro kH pK _) = encode (kH, pK)
-                                      scKeyHash = neighIKeyID
-                                      scSignature = neighISig
-                                      scPushSignature i s = i{neighISig = s}
-instance IntroClass NeighIntro where icPubKey = neighIPubKey
+                                      scKeyHash = _neighISource
+                                      scSignature = _neighISig
+                                      scPushSignature i s = i{_neighISig = s}
+instance IntroClass NeighIntro where icPubKey = _neighIPubKey
 
-instance SignedClass NeighData  where scHash (NeighData  kH _ pay) = encode (kH, pay)
-                                      scKeyHash = neighDKeyID
-                                      scSignature = neighDSig
-                                      scPushSignature d s = d{neighDSig = s}
+instance SignedClass NeighData  where scHash (NeighData src dst _ pay) = encode (src,dst, pay)
+                                      scKeyHash = _neighDSource
+                                      scSignature = _neighDSig
+                                      scPushSignature d s = d{_neighDSig = s}
 
-instance SignedClass PipeHeader where scHash (PipeHeader kH _ n b f) = encode (kH, n, b, f)
-                                      scKeyHash = pipeDKeyID
-                                      scSignature = pipeDSig
-                                      scPushSignature p s = p{pipeDSig = s}
+instance SignedClass PipeHeader where scHash (PipeHeader id _ flags) = encode (id, flags)
+                                      scKeyHash = _pipeDKeyID
+                                      scSignature = _pipeDSig
+                                      scPushSignature p s = p{_pipeDSig = s}
 
-instance SignedClass PipePacket  where scHash (PipePacket h p) = B.append (scHash h)  (encode p)
-                                       scKeyHash = scKeyHash . pipePacketHeader
-                                       scSignature = pipeDSig . pipePacketHeader
-                                       scPushSignature p s = p{pipePacketHeader = scPushSignature (pipePacketHeader p) s}
+instance SignedClass PipePacket  where scHash (PipePacket src dst h p) = B.concat [encode $ (src,dst), (scHash h), (encode p)]
+                                       scKeyHash = scKeyHash . _pipePacketHeader
+                                       scSignature = _pipeDSig . _pipePacketHeader
+                                       scPushSignature p s = over pipePacketHeader (flip scPushSignature s) p
+--                                       scPushSignature p s = p{pipePacketHeader = scPushSignature (pipePacketHeader p) s}
 
 instance Show NeighIntro where
-    show = ("NeighIntro from : " ++ ) . show . neighIKeyID
+    show = ("NeighIntro from : " ++ ) . show . _neighISource
 
 instance Show NeighData where
-    show (NeighData uID _ c) = "NeighData from : " ++ show uID ++ " CONTENT : " ++ show c
+    show (NeighData uID _ _ c) = "NeighData from : " ++ show uID ++ " CONTENT : " ++ show c
 
 
 
