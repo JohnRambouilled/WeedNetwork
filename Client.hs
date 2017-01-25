f6923bcc5b9af651a398a5572aa5b4c8d7a68a4a
JohnRambouilled <you@example.com>
2017-01-26 00:40:07 +0100

    test git

--- Client.hs
+++ Client.hs
@@ -1,12 +1,13 @@
 module Client where
 
+import           Client.Crypto 
+import           Client.Sender
 
-import           Client.Communication
-import           Client.Crypto
-import           Client.Destinary
-import           Client.Graph
-import           Client.Neighbours
-import           Client.Pipes
-import           Client.Relay
-import           Client.Timer
+--import           Client.Communication
+--import           Client.Destinary
+--import           Client.Graph
+--import           Client.Neighbours
+--import           Client.Pipes
+--import           Client.Relay
+--import           Client.Timer
 
